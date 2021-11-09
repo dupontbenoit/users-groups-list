690d8a3116f8c314ef9ef0e76d2d29e5400eb4a3
Benoit Dupont <dupont.benoit@gmail.com>
2021-11-09 17:16:22 -0400

    add: fix model symetricla relationship

--- ug/models.py
+++ ug/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Group(models.Model):
     name = models.CharField(max_length=254, unique=True)
-    parent = models.ManyToManyField("self", blank=True)
+    parent = models.ManyToManyField("self", blank=True, symmetrical=False)
 
     def __str__(self) -> str:
         return self.name
