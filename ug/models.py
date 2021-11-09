85586351ad7ba084ef07c0940ca8cf2ca4eef710
Benoit Dupont <dupont.benoit@gmail.com>
2021-11-09 15:15:23 -0400

    add: models of user and group

--- ug/models.py
+++ ug/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email_address = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=254)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=254, unique=True)
