d2670bee3620b517a2991f95b93c88b1479f34ed
Benoit Dupont <dupont.benoit@gmail.com>
2021-11-09 15:40:42 -0400

    init model relationship

--- ug/models.py
+++ ug/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 
+class Group(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    parent = models.ManyToManyField("self", blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class User(models.Model):
     email_address = models.EmailField(unique=True)
     display_name = models.CharField(max_length=254)
+    groups = models.ManyToManyField(Group)
 
-
-class Group(models.Model):
-    name = models.CharField(max_length=254, unique=True)
+    def __str__(self) -> str:
+        return self.display_name
